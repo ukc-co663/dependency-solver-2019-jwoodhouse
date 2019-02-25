88eafd8be3f3028d758521a119e2254f17470e35
jw728 <jw728@kent.ac.uk>
2019-02-25 06:02:24 +0000

    move to SAT solver

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -5,13 +5,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
+
+import org.logicng.datastructures.Assignment;
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.formulas.Variable;
+import org.logicng.io.parsers.ParserException;
+import org.logicng.io.parsers.PropositionalParser;
+import org.logicng.solvers.MiniSat;
+import org.logicng.solvers.SATSolver;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Sets;
 
 
@@ -37,7 +46,7 @@ class Package {
 		return size;
 	}
 
-	public List<List<String>> getDepends() 
+	public List<List<String>> getDepends()
 	{
 		return depends;
 	}
@@ -71,7 +80,59 @@ class Package {
 	{
 		this.conflicts = conflicts;
 	}
+
+	public String toString()
+	{
+		return (this.getName() + this.getVersion().replace(".", ""));
+	}
 }
+
+class PackageImproved {
+	  private String name;
+	  private String version;
+	  private Integer size;
+	  private List<List<Package>> depends = new ArrayList<>();
+	  private List<Package> conflicts = new ArrayList<>();
+	  
+	  public PackageImproved(String name, String version, Integer size, List<List<Package>> depends, List<Package> conflicts)
+	  {
+		  this.name = name;
+		  this.version = version;
+		  this.size = size;
+		  this.depends = depends;
+		  this.conflicts = conflicts;
+	  }
+
+	  public String getName() 
+	  {
+		  return name;
+	  }
+
+	  public String getVersion() 
+	  {
+		  return version;
+	  }
+
+	  public Integer getSize() 
+	  {
+		  return size;
+	  }
+
+	  public List<List<Package>> getDepends() 
+	  {
+		  return depends;
+	  }
+
+	  public List<Package> getConflicts() 
+	  {
+		  return conflicts;
+	  }
+	  
+	  public String toString()
+	  {
+		  return (this.getName() + this.getVersion().replace(".", ""));
+	  }
+	}
 
 public class Main {
   public static void main(String[] args) throws IOException 
@@ -82,37 +143,96 @@ public class Main {
     
     TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
     
-    List<String> initialState = JSON.parseObject(readFile(args[1]), strListType);
+    List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
     List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
-
-    // CHANGE CODE BELOW:
-    // using repo, initial and constraints, compute a solution and print the answer
-
-    for (Package repoPackage : repository) {
-    	System.out.printf("package %s version %s\n", repoPackage.getName(), repoPackage.getVersion());
-
-    	for (List<String> depClause : repoPackage.getDepends()) {
-    		System.out.printf("  dependency:");
-
-    		for (String dependency : depClause) {
-    			System.out.printf(" %s", dependency);
-    		}
-
-    		System.out.printf("\n");
-    	}
-
-    	for (String confClause : repoPackage.getConflicts()) {
-    		System.out.printf("  conflict: %s\n", confClause);
-    	}
-    } 
     
-    List<Package> packagesToInst = getPackageFromConstraints(constraints, repository);
-    Set<Package> relevantPackages = Sets.newHashSet(getValidRepository(packagesToInst, repository));
     
-    for(Package p : relevantPackages)
+    List<PackageImproved> improvedRepository = buildImprovedRepository(repository);
+    List<List<Package>> positiveConstraints = getPackagesFromConstraints(constraints, '+', repository);
+    List<List<Package>> negativeConstraints = getPackagesFromConstraints(constraints, '-', repository);
+    List<Package> initialState = getPackagesFromInitialState(initial, repository);
+    
+    for(PackageImproved p : improvedRepository)
     {
     	System.out.printf("package %s version %s\n", p.getName(), p.getVersion());
+    	
+    	for(List<Package> deps : p.getDepends())
+    	{
+    		System.out.print("DEP: ");
+    		for(Package dp : deps)
+    		{
+    			System.out.printf("[package %s version %s] ", dp.getName(), dp.getVersion());
+    		}
+    		System.out.print("\n");
+    	}
+    	
+    	for(Package conf : p.getConflicts())
+    	{
+    		System.out.printf("CONF: [package %s version %s]\n", conf.getName(), conf.getVersion());
+    	}
+    	System.out.print("\n");
     }
+    
+    for(List<Package> pc : positiveConstraints)
+    {
+    	System.out.print("+CONSTRAINT: ");
+    	for(Package ppc : pc)
+    	{
+    		System.out.printf("[package %s version %s]", ppc.getName(), ppc.getVersion());
+    	}
+    	System.out.print("\n");
+    	
+    }
+    
+    for(List<Package> nc : negativeConstraints)
+    {
+    	System.out.print("-CONSTRAINT: ");
+    	for(Package nnc : nc)
+    	{
+    		System.out.printf("[package %s version %s]", nnc.getName(), nnc.getVersion());
+    	}
+    	System.out.print("\n");
+    }
+    
+    for(Package ic : initialState)
+    {
+    	System.out.printf("INSTALLED: package %s version %s\n", ic.getName(), ic.getVersion());
+    }
+    
+    final FormulaFactory f = new FormulaFactory();
+	final PropositionalParser p = new PropositionalParser(f);
+	Formula formula = null;
+	
+	List<String> listOfExpressions = new ArrayList<>();
+	
+	
+	
+	
+	
+	try {
+		formula = p.parse("(X11 & ((X21) | (X31) | (X41 & (X11 | X31 | (X51)))) & ((X21) | (X40 & ~X41) | (X50 & ~X51)) & ((X30 & ~X31) | (X40 & ~X41) | (X50 & ~X51)) & ((X20 & ~X21) | (X40 & ~X41) | (X50 & ~X51))) & X11 & X20 & X30 & X41 & X50");
+	} catch (ParserException e) {
+		e.printStackTrace();
+	}
+		
+	final SATSolver miniSat = MiniSat.miniSat(f);
+	miniSat.add(formula);
+	//final Tristate result = miniSat.sat();
+	List<Assignment> pog = miniSat.enumerateAllModels();
+	
+	List<List<PackageImproved>> pogu = getPackagesFromModel(pog, improvedRepository);
+
+	//for(List<PackageImproved> lp : pogu)
+	//{
+	//	System.out.print("SET: ");
+	//	for(PackageImproved pp : lp)
+	//	{
+	//		System.out.printf("[package %s version %s] ", pp.getName(), pp.getVersion());
+	//	}
+	//	System.out.print("\n");
+	//}
+	
+	System.out.println(repositoryToBooleanExp(improvedRepository, improvedRepository.get(0)));
   }
 
   static String readFile(String filename) throws IOException {
@@ -121,64 +241,117 @@ public class Main {
     br.lines().forEach(line -> sb.append(line));
     return sb.toString();
   }
-
-  static List<Package> getPackageFromConstraints(List<String> constraints, List<Package> repository)
+  
+  static String repositoryToBooleanExp(List<PackageImproved> improvedRepository, PackageImproved packageToInstall)
   {
-	  //probably be lazy here and just import every version of the package if there is no version
-	  //cause of uninstall weight.
+	  String booleanExp = packageToBooleanExp(packageToInstall);
 	  
-	  //also need to handle uninstalls at some stage. not sure how :(
-	  /**
-	   * FOR EACH CONSTRAINT
-	   * 	GET NAME
-	   * 	FOR EACH PACKAGE IN REPO
-	   * 		ADD TO PARSEDCONSTRAINTS AS PACKAGE
-	   * 	END
-	   * END
-	   */
-	  List<Package> parsedConstraints = new ArrayList<>();
+	  for(PackageImproved p : improvedRepository)
+	  {
+		  if(!p.toString().equals(packageToInstall.toString()))
+		  {
+			  booleanExp = booleanExp.replace("[packageIdent]" + p.toString() + "[packageIdent]", packageToBooleanExp(p));
+		  }
+	  }
+	  return booleanExp.replace("[packageIdent]", "");
+  }
+  
+  static String repositoryToBooleanExp2(List<PackageImproved> improvedRepository, List<List<Package>> positiveConstraints, List<List<Package>> negativeConstraints)
+  {
+	  String booleanExp = "";
+	  
+	  
+	  
+	  return null;
+  }
+  
+  static String packageToBooleanExp(PackageImproved p)
+  {
+	  String booleanExp = "(" + "[packageIdent]" + p.toString() + "[packageIdent]";
+
+	  for(List<Package> deps : p.getDepends())
+	  {
+		  booleanExp = booleanExp + " & (";
+		  for(Package dp : deps)
+		  {
+			  booleanExp = booleanExp + "[packageIdent]" + dp.toString() + "[packageIdent]" + " | ";
+		  }
+		  booleanExp = booleanExp.substring(0, booleanExp.length() -3) + ")";
+	  }
+
+	  for(Package conf : p.getConflicts())
+	  {
+		  booleanExp = booleanExp + " & ~" + "[packageIdent]" + conf.toString() + "[packageIdent]";
+	  }
+	  	  
+	  return booleanExp + ")";
+  }
+  
+  static List<PackageImproved> buildImprovedRepository(List<Package> repository)
+  {
+	  List<PackageImproved> improvedRepository = new ArrayList<>();
+	  for (Package p : repository)
+	  {
+		  String name = p.getName();
+		  String version = p.getVersion();
+		  Integer size = p.getSize();
+		  List<List<Package>> depends = new ArrayList<>();
+		  List<Package> conflicts = new ArrayList<>();
+		  
+		  for(List<String> deps : p.getDepends())
+		  {
+			  List<Package> orDepends = new ArrayList<>();
+			  
+			  for(String singleDep : deps)
+			  {
+				  List<Package> strToPackages = getPackagesFromString(singleDep, repository);
+				  for(Package dp : strToPackages)
+				  {
+					  orDepends.add(dp);
+				  }
+				  
+			  }
+			  depends.add(orDepends);
+		  }
+		  
+		  for(String conf : p.getConflicts())
+		  {
+			  List<Package> strToPackages = getPackagesFromString(conf, repository);
+			  for(Package cp : strToPackages)
+			  {
+				  conflicts.add(cp);
+			  }
+		  }
+		  
+		  PackageImproved pi = new PackageImproved(name, version, size, depends, conflicts);
+		  improvedRepository.add(pi);
+	  }
+	  return improvedRepository;
+  }
+  
+  static List<List<Package>> getPackagesFromConstraints(List<String> constraints, Character constraintType, List<Package> repository)
+  {
+	  List<List<Package>> actionConstraints = new ArrayList<>();
 	  
 	  for(String constraint : constraints)
 	  {
-		  if(constraint.contains("="))
+		  if(constraint.charAt(0) == constraintType)
 		  {
-			  String[] nameVersion = constraint.split("=");
-			  String constraintName = nameVersion[0].substring(1);
-			  String version = nameVersion[1];
-
-			  for(Package p : repository)
-			  {
-				  if(p.getName().equals(constraintName) && p.getVersion().equals(version))
-				  {
-					  parsedConstraints.add(p);
-				  }
-			  }
-		  }
-		  else
-		  {
-			  //dont need this?
-			  String latestVersion = "0";
-			  String constraintName = constraint.substring(1);
-			  if(constraint.charAt(0) == '+')
-			  {
-				  for(Package p : repository)
-				  {
-					  if(p.getName().equals(constraintName) && compareVersions(p.getVersion(), latestVersion) > 0)
-					  {
-						  latestVersion = p.getVersion();
-					  }
-				  }
-				  for(Package p : repository)
-				  {
-					  if(p.getName().equals(constraintName) && p.getVersion().equals(latestVersion))
-					  {
-						  parsedConstraints.add(p);
-					  }
-				  }
-			  }
+			  List<Package> subConstraint = getPackagesFromString(constraint.substring(1), repository);
+			 actionConstraints.add(subConstraint);
 		  }
 	  }
-	  return parsedConstraints;
+	  return actionConstraints;
+  }
+  
+  static List<Package> getPackagesFromInitialState(List<String> initial, List<Package> repository)
+  {
+	  List<Package> initialState = new ArrayList<>();
+	  for(String init : initial)
+	  {
+		  initialState.add(getPackagesFromString(init, repository).get(0));
+	  }
+	  return initialState;
   }
 
   static int compareVersions(String versionA, String versionB)
@@ -190,110 +363,30 @@ public class Main {
 	  return versionA.compareTo(versionB);
   }
   
-  static boolean removeConflictCheckSafe(Set<Package> dependencyPackages, Package conflictPackage, List<Package> repository)
+  static List<List<PackageImproved>> getPackagesFromModel(List<Assignment> satModel, List<PackageImproved> improvedRepository)
   {
-	  boolean isSafeToRemove = true;
-	  for(Package p : dependencyPackages)
+	  List<List<PackageImproved>> listOfStates = new ArrayList<>();
+	  
+	  for(Assignment a : satModel)
 	  {
-		  for (List<String> sp : p.getDepends())
+		  List<PackageImproved> state = new ArrayList<>();
+		  for (Variable v : a.positiveLiterals())
 		  {
-			  if(sp.size() == 1)
-			  {
-				  List<Package> pkgToCheck = getPackageTest(sp.get(0), repository);
-				  for(Package pp : pkgToCheck)
-				  {
-					  if(pp == conflictPackage)
-					  {
-						  isSafeToRemove = false;
-					  }
-				  }
-			  }
-		  }  
-	  }
-	  return isSafeToRemove;
-  }
-
-  static Set<Package> getValidRepository(List<Package> parsedConstraints, List<Package> repository)
-  {
-	  List<Package> relevantPackages = new ArrayList<>();
-	  List<Package> dependencyPackages = new ArrayList<>();
-	  List<Package> conflictPackages = new ArrayList<>();
-	  
-	  //Using a queue + iteration to avoid using recursion.
-	  Queue<Package> evaluationQueue = new LinkedList<>();
-	  
-	  for(Package p : parsedConstraints)
-	  {
-		  evaluationQueue.add(p);
-	  }
-	  
-	  //can traverse all dependencies from top to bottom by queueing and dequeueing
-	  //until queue is empty
-	  while(!evaluationQueue.isEmpty())
-	  {
-		  Package p = evaluationQueue.remove();
-		  dependencyPackages.add(p);
-		  //System.out.printf("name: %s version %s\n", p.getName(), p.getVersion());
-		  
-		  for(List<String> dependencies : p.getDepends())
-		  {
-			  for(String dependency : dependencies)
-			  {
-				  for(Package pp : getPackageTest(dependency, repository))
-				  {
-					  if(!dependencyPackages.contains(pp))
-					  {
-						  evaluationQueue.add(pp);
-					  }
-				  }
-			  }
+			for(PackageImproved p : improvedRepository)
+			{
+				if(p.toString().equals(v.toString()))
+				{
+					state.add(p);
+				}
+			}
 		  }
+		  listOfStates.add(state);
 	  }
-	  
-	  //opposite of dependencies, we only dequeue as we know all the packages we care about.
-	  for(Package p : dependencyPackages)
-	  {
-		  evaluationQueue.add(p);
-	  }
-	  
-	  //we only get conflicts for packages in the generated dependency list.
-	  while(!evaluationQueue.isEmpty())
-	  {
-		  Package p = evaluationQueue.remove();
-		  
-		  for(String conflict : p.getConflicts())
-		  {
-			  for(Package pp : getPackageTest(conflict, repository))
-			  {
-				  conflictPackages.add(pp);
-			  }
-		  }
-		  
-	  }
-	  
-	  //easy way to clear duplicates from a List.
-	  Set<Package> conflictPackagesSet = Sets.newHashSet(conflictPackages);
-	  Set<Package> dependencyPackagesSet = Sets.newHashSet(dependencyPackages);
-	  
-	  for(Package c : conflictPackagesSet)
-	  {
-		  if(removeConflictCheckSafe(dependencyPackagesSet, c, repository))
-		  {
-			  dependencyPackagesSet.remove(c);
-		  }
-	  }
-	  
-	  for(Package p : conflictPackagesSet)
-	  {
-		  System.out.printf("CONF: package %s version %s\n", p.getName(), p.getVersion());
-	  }
-	  
-	  //return needs to be changed to relevantPackages.
-	  return dependencyPackagesSet;
+	  return listOfStates;
   }
   
   //converts a string name of package to a list of packages matched against it.
-  static List<Package> getPackageTest(String packageString, List<Package> repository)
+  static List<Package> getPackagesFromString(String packageString, List<Package> repository)
   {
 	  List<Package> packages = new ArrayList<>();
 
@@ -380,6 +473,4 @@ public class Main {
 
 	  return packages;
   }
-  
-  //need a pog method to determine safety of a removal.
 }
